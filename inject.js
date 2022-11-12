0bd5c6facf574434e6d8acabc0b243211985c625
北條佑弥 <y.hojo@core-tech.jp>
2022-11-12 18:00:00 +0900

    うまくいかないがひとまず...

--- inject.js
+++ inject.js
@@ -1,28 +1,31 @@
-function codeToInject() {
-    var propagateToExtension = () => {
-        document.dispatchEvent(new CustomEvent('ErrorToExtension', {}));
-    }
+document.addEventListener("DOMContentLoaded", event => {
+    function codeToInject() {
+        var propagateToExtension = () => {
+            document.dispatchEvent(new CustomEvent('ErrorToExtension', {}));
+        }
 
-    window.addEventListener('error', propagateToExtension, false);    
-    window.addEventListener('unhandledrejection', propagateToExtension, false);
+        window.addEventListener('error', propagateToExtension, false);    
+        window.addEventListener('unhandledrejection', propagateToExtension, false);
 
-    var consoleErrorFunc = window.console.error;
-    window.console.error = function() {
-        consoleErrorFunc.apply(console, arguments);
-        propagateToExtension();
+        var consoleErrorFunc = window.console.error;
+        window.console.error = function() {
+            consoleErrorFunc.apply(console, arguments);
+            propagateToExtension();
+        }
     }
-}
+});
 
 (function() {
     var eci_error_count = 0;
 
-    var script = document.createElement('script');
-    script.textContent = '(' + codeToInject + '())';
-    (document.head || document.documentElement).appendChild(script);
-    script.parentNode.removeChild(script);
+    // var script = document.createElement('script');
+    // script.textContent = '(' + codeToInject + '())';
+    // console.log(script);
+    // (document.head || document.documentElement).appendChild(script);
+    // script.parentNode.removeChild(script);
 
     document.addEventListener('ErrorToExtension', function(e) {
         eci_error_count++;
         chrome.runtime.sendMessage({error: eci_error_count.toString()});
     });
-})()
+})();
